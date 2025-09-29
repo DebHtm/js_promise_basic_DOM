bcaeaca5107aee826557c8adbb0b2948d2bbf213
Denys <yolm9yom@gmail.com>
2025-09-29 18:19:05 +0300

    promise1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,42 @@
 'use strict';
 
-// write your code here
+const promise1 = new Promise((resolve, reject) => {
+  const logo = document.querySelector('.logo');
+
+  logo.addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
+});
+
+function usePromise2() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject(new Error('Promise was rejected!'));
+    }, 3000);
+  });
+}
+
+function funcSuccess(message) {
+  const div = document.createElement('div');
+
+  div.className = 'message';
+  div.textContent = message;
+  document.body.appendChild(div);
+}
+
+function funcError(error) {
+  const div = document.createElement('div');
+
+  div.className = 'message error-message';
+  div.textContent = error.message;
+  document.body.appendChild(div);
+}
+
+promise1
+  .then((msg) => {
+    funcSuccess(msg);
+
+    return usePromise2();
+  })
+  .then(funcSuccess)
+  .catch(funcError);
